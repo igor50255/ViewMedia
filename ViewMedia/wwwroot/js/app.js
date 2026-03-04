94aafbc203e204d70782ba09c64bfec4d30ccc85
Igor Sicorskiy <idriveview@gmail.com>
2026-03-04 21:51:58 +0300

    Заполнение выпадающего списка и меню реальными данными с пк: выпадающее меню - папки первого ряда, меню - папки второго ряда. Сделал для всего всплывающие подсказки (нужны для длинных имён папок)

--- ViewMedia/wwwroot/js/app.js
+++ ViewMedia/wwwroot/js/app.js
@@ -1,45 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
   // отправка запроса для получения актуального пути к папке с контентом для отображения в программе
-  const payload = { type: 'get-path-content' };
-  chrome.webview.postMessage(payload);
+  const pathFolder = { type: 'get-path-content' };
+  chrome.webview.postMessage(pathFolder);
 
-  const menuItems = [
-    "Главная",
-    "Услуги",
-    "Контакты",
-    "Contacts"
-  ];
-  const folders = [
-    "Genesis 9",
-    "Genesis 8 - 8.1",
-    "Genesis 3",
-    "Genesis 2",
-    "Victoria 4",
-    "Other"
-  ];
-  // заполнение меню и выпадающего списка данными
-  fiilingSelectionFolders(menuItems, folders);
-
-  window.chrome.webview.addEventListener('message', (e) => {
-    const msg = typeof e.data === 'string' ? JSON.parse(e.data) : e.data;
-    // отображение картинок превью контента
-    if (msg.type == 'images') {
-      let images = msg.data;
-      console.log(images); // проверка контента
-    }
-    // получение актуального пути к папке с контентом
-    else if (msg.type == 'set-path-content') {
-      let path = msg.pathContent;
-      // прописывам актуальный путь 
-      const content = document.querySelector('#path-content');
-      content.textContent = path;
-    }
-    // отключение оверлея загрузки
-    else if (msg.type == 'finish-overlay') {
-      hideLoader();
-    }
-    else return;
+  // отправка запроса для получения первоначальных данных для заполнения списков выбора папки
+  const pathFolders = { type: 'get-path-folders' };
+  chrome.webview.postMessage(pathFolders);
 
 
-  });
 });
+
+window.chrome.webview.addEventListener('message', (e) => {
+  const msg = typeof e.data === 'string' ? JSON.parse(e.data) : e.data;
+  // отображение картинок превью контента
+  if (msg.type == 'images') {
+    let images = msg.data;
+    console.log(images); // проверка контента
+  }
+  // получение актуального пути к папке с контентом
+  else if (msg.type == 'set-path-content') {
+    let path = msg.pathContent;
+    // прописывам актуальный путь 
+    const content = document.querySelector('#path-content');
+    content.textContent = path;
+  }
+  // получение массива папок и заполнение списков выбора
+  else if (msg.type == 'set-path-folders') {
+    let directories = msg.pathFolders;
+    // заполнение меню и выпадающего списка данными
+    if (directories) {
+      fiilingSelectionFolders(msg.pathItems, directories);
+    }
+    console.log("sdfasdf: " + directories);
+  }
+  // получение массива папок в заданной директории
+  else if (msg.type == 'set-path-second-folders') {
+    let menuItems = msg.pathFolders;
+    // заполнение меню и выпадающего списка данными
+    if (menuItems) {
+      // Заполнение меню-гамбургер (слева)
+      fiilingMenuItems(menuItems);
+    }
+  }
+  // отключение оверлея загрузки
+  else if (msg.type == 'finish-overlay') {
+    hideLoader();
+  }
+  else return;
+
+});
+
