3ffca5d66ff20d0267d91e97badccd848373da28
Igor Sicorskiy <idriveview@gmail.com>
2026-03-23 14:55:29 +0300

    Добавлено логгирование. Логгируется: дабавление и удаление превью

--- ViewMedia/App.xaml.cs
+++ ViewMedia/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Serilog;
+using System.Windows;
 
 namespace ViewMedia;
 
@@ -15,6 +16,26 @@ public partial class App : Application
         {
             //MessageBox.Show("RESTART MODE");
         }
+
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File(
+                path: "logs/app-.log",
+                rollingInterval: RollingInterval.Day,   // новый файл каждый день
+                fileSizeLimitBytes: 26214400,
+                rollOnFileSizeLimit: true,
+                retainedFileCountLimit: 7,
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}"
+            )
+            .CreateLogger();
+
+        Log.Information(" ----- Приложение запущено -----");
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        Log.CloseAndFlush(); // важно — сбросить буфер при выходе
+        base.OnExit(e);
     }
 }
 
